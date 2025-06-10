164cddefc41a5a6a54c8e09e935aed01d1546c5b
UP240259 <up240259@alumnos.upa.edu.mx>
2025-06-10 10:18:35 -0600

    dia 2 completado

--- Day 2.py
+++ Day 2.py
@@ -1,4 +1,70 @@
 #Day 2: 30 days of python programming'
 n='josé'
 print("tu nombre es: ",n)
+l= "Hernéndez Gutiérrez"
+print("y tu apellido es:", l)
+c= "José de Jesús Hernández Gutiérrez"
+print("tu nombre completo es:",c)
+country= "México"
+print("Tu país es:",country)
+city= "Aguascalientes"
+print("La ciudad es: ", city)
+age= "19"
+print("tu edad es:", age)
+year="2025"
+print("el año es: ",year)
+is_married= False
+is_true= True
+is_light_on= True
+canción, album, artista= "cut my lip", "trench", "Twenty one pilots"
+print("canción:",canción, "album: ", album,  "artista: ", artista)
+
+#clase de las variables utilizadas.
+print(type(n))
+print(type(l))
+print(type(c))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+
+print("longitud del nombre: ", len(n))  
+print("longitud del apellido", len(l))
+num_one= 5
+num_two= 4
+total= num_one+num_two
+print("la suma de los valores es= ", total)
+subtract= num_one-num_two
+print("la resta de los valores es: ", subtract)
+multiplicacion= num_one*num_two
+print("la multiplicación de los valores es: ", multiplicacion)
+divicion= num_one/num_two
+print("la división de los valores es: ", divicion)
+modulus= num_two/num_one
+print("la divición de los valores es: ", modulus)
+exp= num_one**num_two
+print("el exponencial del número uno es: ", exp)
+floor= (num_one//num_two)
+print("la divición de los valores es: ", floor)
+area_of_circle= 3.1416*(30**2)
+print("el area de un circulo es: ", area_of_circle)
+circunferencia= 2*(3.1416)*(30)
+print("la circunferencia del circulo es: ", circunferencia)
+radius = float(input("Input the radius of the circle: "))
+area = 3.1416* radius ** 2
+print("The area of the circle with radius", radius, "is:", area)
+nombre= input("escribe tu nombre: ")
+apellido= input("escribe tus apellidos: ")
+edad= input("escribe tu edad: ")
+pais=input("escribe tu pais: ")
+
+
+
+
+
+
 
